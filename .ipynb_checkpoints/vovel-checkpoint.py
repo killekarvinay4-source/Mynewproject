213c747b168e32f2584475eb2d78d7b672461db9
AMD <killekarvinay4@gmail.com>
2026-05-17 05:08:29 -0700

    C4 vovel code is written

---
+++ .ipynb_checkpoints/vovel-checkpoint.py
@@ -0,0 +1,11 @@
+# vovel count
+
+def vovel_count(text):
+    vovels = "aeiouAEIOU"
+    count = 0
+    for char is text:
+        if char in vovel:
+        count += 1
+    return count
+    print(vowe)_count("Hello world"))
+    
